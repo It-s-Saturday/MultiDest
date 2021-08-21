c43983cc8a6395fb7ee84c58730c1496825af84b
jibranabsarulislam <jga26@njit.edu>
2021-08-21 01:01:02 -0400

    nodify the logic

--- logic/compute.py
+++ logic/compute.py
@@ -1,66 +1,33 @@
 # grab list of inputs from an expandable form (max 5 to start)
-    # ignore empty inputs
-    # try and confirm lookup, maybe directly reference google api to confirm location
-        # above solution should not require any error handling
+# ignore empty inputs
+# try and confirm lookup, maybe directly reference google api to confirm location
+# above solution should not require any error handling
 
 # each list element then is represented as a node that needs to check distance to each other node
-    # based on distance and time
+# based on distance and time
 
 #
-
-class Node:
-    def __init__(self, name):
-        self.name = name
-        self.distance_from_origin = None
-        self.distances_to_others = []
-        self.distance_to_destination = None
-
-        self.isOrigin = None
-        self.isDestination = None
-
-    def setAsOrigin(self):
-        self.isOrigin = True
-
-    def setAsDestination(self):
-        self.isDestination = True
-
-    def unsetAsOrigin(self):
-        self.isOrigin = False
-
-    def unsetAsDestination(self):
-        self.isDestination = False
-
-    def getName(self):
-        return self.name
-
-    def getDistanceFromOrigin(self):
-        return self.distance_from_origin
-
-    def getDistancesToOthers(self):
-        return self.distances_to_others
-
-    def getDistanceToDestination(self):
-        return self.distance_to_destination
-
-    def __str__(self):
-        return "Node:" + self.name
-
-
-origin = ["Home"]
-destination = ["CFA"]
-
-points = ["school", "LJ"]
+import Node
 
 travel = {}
 
+originNode = Node.Node("Home")
+originNode.setAsOrigin()
 
-travel[Node(origin[0])] = 0
+destinationNode = Node.Node("CFA")
+destinationNode.setAsDestination()
 
-for point in points:
+stops = ["school", "LJ"]
 
-    travel[Node(point)] = None
+travel[originNode] = 0
 
-travel[Node(destination[0])] = len(points) + 1
+list_of_stops = []
+for stop in stops:
+    temp = Node.Node(stop)
+    travel[temp] = None
+    list_of_stops.append(temp)
+
+travel[destinationNode] = len(stops) + 1
 
 for key in travel.keys():
-    print(key)
+    print(key.__str__(), travel[key])
