84fc4d28e4c67e3a79b23c4c2e358731d1d96807
jibranabsarulislam <jga26@njit.edu>
2021-08-21 04:13:03 -0400

    logic done

--- logic/tps.py
+++ logic/tps.py
@@ -4,12 +4,21 @@ import Node
 
 
 def tps(graph, origin, destination):  # LOOKING FOR 22
-    print(graph)
-    path = infinity
-    for i in permutations(graph):
-        cost = 0
-        if i[0] != origin or i[-1] != destination:
+    evaluate = []
+    for permutation in permutations(graph):
+        if permutation[0] != origin or permutation[-1] != destination:
             continue
         # implement linked-listesque methodology
-
+        evaluate.append(permutation)
+    print(evaluate)
+    cost = infinity
+    for path in evaluate:
+        i = 0
+        c = 0
+        while path[i] != path[-1]:
+            c += graph[path[i]][path[i+1]]
+            i += 1
+        cost = min(cost, c)
+        print("\n")
+    print(cost)
 
