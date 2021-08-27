31a5ac96d94d246b68ad913056b70402ec4f280d
jibranabsarulislam <jga26@njit.edu>
2021-08-27 00:21:38 -0400

    react app now being rendered through django

--- multidestdjango/multidestdjango/settings.py
+++ multidestdjango/multidestdjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'multidestdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                 os.path.join(BASE_DIR, '../multidest-app/build'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join("""../multidest-app/build/static"""),
+]
---
+++ multidestdjango/multidestdjango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+# def simple_function(request):
+#     print("\nThis is a simple function\n")
+#     return HttpResponse("""<html><script>window.location.replace('/');</script></html>""")
