3b1debf8813c57cc1fd78a88e813bac6912682dc
jibranabsarulislam <jga26@njit.edu>
2021-08-30 23:57:02 -0400

    back to terminal

---
+++ multidestdjango/multidestdjango/logic/ignoremepls/test.py
@@ -0,0 +1,15 @@
+
+def add(first, second):
+    return first+second
+
+def multiply(first, second):
+    return first*second
+
+print(multiply(add(2, 2), add(3, 3)))
+
+                # 4         # 6
+
+
+
+
+
